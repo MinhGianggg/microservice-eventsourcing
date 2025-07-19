5c07327a9f5347a6cded9ab85412373442fbb57d
v-minhgh [VFVN-PN] <pn.minhgh@vingroup.net>
2025-07-19 14:16:54 +0700

    validate request body

--- common-service/src/main/java/com/minhgh/common/service/advice/ExceptionAdvice.java
+++ common-service/src/main/java/com/minhgh/common/service/advice/ExceptionAdvice.java
@@ -2,17 +2,37 @@ package com.minhgh.common.service.advice;
 
 import com.minhgh.common.service.model.ErrorMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (var error : ex.getBindingResult().getAllErrors()) {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            errors.put(fieldName, message);
+        }
+
+        return errors;
+    }
+
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleException(Exception ex) {
         return new ErrorMessage(
-            "9999", ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                "9999", ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
