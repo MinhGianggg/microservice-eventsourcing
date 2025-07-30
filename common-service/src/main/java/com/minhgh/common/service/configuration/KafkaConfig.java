296cf0ca034bb7c0e7a5bb538644af43f8f5ece9
v-minhgh [VFVN-PN] <pn.minhgh@vingroup.net>
2025-07-30 13:41:24 +0700

    add kafka retry

--- common-service/src/main/java/com/minhgh/common/service/configuration/KafkaConfig.java
+++ common-service/src/main/java/com/minhgh/common/service/configuration/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.minhgh.common.service.configuration;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
